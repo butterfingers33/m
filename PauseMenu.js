85e035d3e26794591fc32bd4ea130fd7b15adb09
guitarhero22 <rushabhkanadia@gmail.com>
2022-05-29 03:10:48 +0530

    Trying to add a menu :(

---
+++ PauseMenu.js
@@ -0,0 +1,22 @@
+// class PauseMenu {
+//     constructor({onComplete}) {
+//         this.onComplete = onComplete;
+//     }
+
+//     createElement() {
+//         this.element = document.createElement("div");
+//         this.element.classList.add("PauseMenu");
+
+//         this.element.innerHTML = (`
+//             <h2>Pause Menu</h2>
+//         `)
+        
+//     }
+
+//     init(container) {
+//         this.createElement();
+//         this.keybaordMeny = new this.keybaordMeny({
+//             //
+//         })
+//     }
+// }
